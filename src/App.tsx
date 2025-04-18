aea1ccf6b8e858a28f6910b6829d8c25ba346daf
Pedro <pedrodevnetwork01@gmail.com>
2025-04-18 01:04:30 -0300

    feat(ui): add dark/light theme toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 import "./global.css";
 import { router } from "./routes";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <title>Dashboard | pizza.shop</title>
       <Toaster />
       <RouterProvider router={router} />
-    </>
+    </ThemeProvider>
   );
 }
