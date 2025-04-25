291634120df614cff48cd81bca7a059841de68da
Pedro <pedrodevnetwork01@gmail.com>
2025-04-24 23:12:18 -0300

    chore: install @tanstack/react-query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-provider";
 import "./global.css";
+import { queryClient } from "./lib/react-query";
 import { router } from "./routes";
 
 export function App() {
@@ -9,7 +11,9 @@ export function App() {
     <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
       <title>Dashboard | pizza.shop</title>
       <Toaster />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
