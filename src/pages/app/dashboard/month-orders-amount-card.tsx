99d37910989335166d06eb62dc23000ce62291c5
Pedro <pedrodevnetwork01@gmail.com>
2025-05-06 10:26:33 -0300

    feat(dashboard): add skeleton loading for metrics data

--- src/pages/app/dashboard/month-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-orders-amount-card.tsx
@@ -2,6 +2,7 @@ import { getMonthOrdersAmount } from "@/api/get-month-orders-amount";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
 import { TrendingDown, TrendingUp, Utensils } from "lucide-react";
+import { MetricCardSkeleton } from "./metric-card-skeleton";
 
 // export interface MonthRevenueCardProps {}
 
@@ -18,7 +19,7 @@ export function MonthOrdersAmountCard() {
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthOrdersAmount && (
+        {monthOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {monthOrdersAmount.amount.toLocaleString("pt-BR")}
@@ -44,6 +45,8 @@ export function MonthOrdersAmountCard() {
               relação ao mês passado
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
--- src/pages/app/dashboard/month-revenue-card.tsx
+++ src/pages/app/dashboard/month-revenue-card.tsx
@@ -2,6 +2,7 @@ import { getMonthRevenue } from "@/api/get-month-revenue";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
 import { DollarSign, TrendingDown, TrendingUp } from "lucide-react";
+import { MetricCardSkeleton } from "./metric-card-skeleton";
 
 // export interface MonthRevenueCardProps {}
 
@@ -20,7 +21,7 @@ export function MonthRevenueCard() {
         <DollarSign className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthRevenue && (
+        {monthRevenue ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {(monthRevenue.receipt / 100).toLocaleString("pt-BR", {
@@ -49,6 +50,8 @@ export function MonthRevenueCard() {
               relação a ontem
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
